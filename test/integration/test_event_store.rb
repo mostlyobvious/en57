096ffdd65e1543b677e581f12e7b49c7346c8a24
Paweł Pacana <pawel.pacana@gmail.com>
2026-04-14 22:42:23 +0200

    Integration-test EventStore against Postgres
    - Append and read two events through a real PG connection
    - Check unit tests aren't lying about the wire format
    - DATABASE_URL overrides default postgres:///en57_test
    - Schema is created inline and truncated before each run

---
+++ test/integration/test_event_store.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require "test_helper"
+require "pg"
+
+class IntegrationTestEventStore < Minitest::Test
+  def setup
+    @connection = PG.connect(ENV.fetch("DATABASE_URL", "postgres:///en57_test"))
+    @connection.exec("CREATE TABLE IF NOT EXISTS events (type TEXT NOT NULL, data JSONB NOT NULL)")
+    @connection.exec("TRUNCATE events")
+    @event_store = En57::EventStore.new(@connection)
+  end
+
+  def teardown
+    @connection&.close
+  end
+
+  def test_writes_and_reads_two_events
+    @event_store.append(
+      [
+        En57::Event.new(type: "CredditToppedUp", data: {amount: 100}),
+        En57::Event.new(type: "CredditToppedUp", data: {amount: 50})
+      ]
+    )
+
+    assert_equal(
+      [
+        En57::Event.new(type: "CredditToppedUp", data: {"amount" => 100}),
+        En57::Event.new(type: "CredditToppedUp", data: {"amount" => 50})
+      ],
+      @event_store.read
+    )
+  end
+end
