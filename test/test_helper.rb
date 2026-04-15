fad2341f16de464648cd040fff440fd680d57f37
Paweł Pacana <pawel.pacana@gmail.com>
2026-04-15 22:25:07 +0200

    Drop redundant LOAD_PATH extension
    - Not needed for bin/rake
    - Not needed for m runner
    - Running tests via ruby would need -Itest:lib as of now

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
-$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
-
-require "en57"
-
 require "minitest/autorun"
 require "minitest/mock"
 require "mutant/minitest/coverage"
+
+require "en57"
