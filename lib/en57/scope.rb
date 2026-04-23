ed1de18ee52a752129243c0126e76502cab013e8
Paweł Pacana <pawel.pacana@gmail.com>
2026-04-23 19:31:07 +0200

    Refactor

--- lib/en57/scope.rb
+++ lib/en57/scope.rb
@@ -1,42 +1,25 @@
 # frozen_string_literal: true
 
 module En57
-  class Scope
-    def initialize(repository, query)
-      @repository = repository
-      @query = query
+  Scope =
+    Data.define(:repository, :query) do
+      def each(&block)
+        return enum_for unless block
+
+        repository.read(query).each(&block)
+      end
+
+      def with_tag(**tags)
+        with(query: query.refine_last { |item| item.with_tags(tags) })
+      end
+
+      def of_type(*types)
+        with(query: query.refine_last { |item| item.with_types(types) })
+      end
+
+      def or(other)
+        with(query: query.or(other.query))
+      end
+      alias_method :|, :or
     end
-
-    def each(&block)
-      return enum_for unless block
-
-      @repository.read(@query).each(&block)
-    end
-
-    def with_tag(**tags)
-      self.class.new(
-        @repository,
-        @query.refine_last { |item| item.with_tags(tags) },
-      )
-    end
-
-    def of_type(*types)
-      self.class.new(
-        @repository,
-        @query.refine_last { |item| item.with_types(types) },
-      )
-    end
-
-    def or(other)
-      self.class.new(@repository, @query.or(other.query))
-    end
-
-    def |(other)
-      self.or(other)
-    end
-
-    protected
-
-    attr_reader :query
-  end
 end
