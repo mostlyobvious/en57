c43cd221373f0eb24e74d73c397574970444e16e
Paweł Pacana <pawel.pacana@gmail.com>
2026-04-24 10:02:57 +0200

    Formatting

--- db/schema.sql
+++ db/schema.sql
@@ -33,15 +33,14 @@ CREATE FUNCTION append_events (new_events event_with_tags[])
         e.meta
     FROM
         unnest(new_events) AS e;
-
-    INSERT INTO tags (event_id, key, value)
-    SELECT
-        e.id,
-        t.key,
-        t.value
-    FROM
-        unnest(new_events) AS e
-        CROSS JOIN LATERAL jsonb_each_text(COALESCE(e.tags, '{}'::jsonb)) AS t;
+INSERT INTO tags (event_id, key, value)
+SELECT
+    e.id,
+    t.key,
+    t.value
+FROM
+    unnest(new_events) AS e
+    CROSS JOIN LATERAL jsonb_each_text(COALESCE(e.tags, '{}'::jsonb)) AS t;
 $$;
 
 CREATE FUNCTION read_events (tag_filters jsonb[], type_filters jsonb[])
@@ -57,61 +56,48 @@ CREATE FUNCTION read_events (tag_filters jsonb[], type_filters jsonb[])
             e.meta
         FROM
             events AS e
-        WHERE
-            (cardinality(tag_filters) = 0 AND cardinality(type_filters) = 0)
-            OR EXISTS (
-                SELECT
-                    1
-                FROM
-                    generate_subscripts(tag_filters, 1) AS idx
-                WHERE
-                    (
-                        tag_filters[idx] = '{}'::jsonb
-                        OR NOT EXISTS (
+        WHERE (cardinality(tag_filters) = 0
+            AND cardinality(type_filters) = 0)
+        OR EXISTS (
+            SELECT
+                1
+            FROM
+                generate_subscripts(tag_filters, 1) AS idx
+            WHERE (tag_filters[idx] = '{}'::jsonb
+                OR NOT EXISTS (
+                    SELECT
+                        1
+                    FROM
+                        jsonb_each_text(tag_filters[idx]) AS f (key,
+                            value)
+                    WHERE
+                        NOT EXISTS (
                             SELECT
                                 1
                             FROM
-                                jsonb_each_text(tag_filters[idx]) AS f (key, value)
+                                tags AS t
                             WHERE
-                                NOT EXISTS (
-                                    SELECT
-                                        1
-                                    FROM
-                                        tags AS t
-                                    WHERE
-                                        t.event_id = e.id
-                                        AND t.key = f.key
-                                        AND t.value = f.value
-                                )
-                        )
-                    )
-                    AND (
-                        type_filters[idx] = '[]'::jsonb
+                                t.event_id = e.id
+                                AND t.key = f.key
+                                AND t.value = f.value)))
+                    AND (type_filters[idx] = '[]'::jsonb
                         OR e.type IN (
                             SELECT
-                                jsonb_array_elements_text(type_filters[idx])
-                        )
-                    )
-            )
-    )
-    SELECT
-        e.id,
-        e.type,
-        e.data,
-        e.meta,
-        COALESCE(
-            (
+                                jsonb_array_elements_text(type_filters[idx])))))
+        SELECT
+            e.id,
+            e.type,
+            e.data,
+            e.meta,
+            COALESCE((
                 SELECT
                     jsonb_object_agg(t.key, t.value)
-                FROM
-                    tags AS t
-                WHERE
-                    t.event_id = e.id
-            ),
-            '{}'::jsonb
-        )
+            FROM tags AS t
+        WHERE
+            t.event_id = e.id), '{}'::jsonb)
     FROM
         filtered_events AS e
     ORDER BY
         e.position;
 $$;
+
